96fec369ddda0aa8d11ece7cd903d3719482814b
v-e-n-u-s-o-s <janekpieklo@icloud.com>
2021-04-15 13:42:44 +0200

    Update

--- boardgenerator.js
+++ boardgenerator.js
@@ -1,36 +1,54 @@
 var maxboardsizexPROMPT = Number(prompt("Podaj szerokość planszy"));
 var maxboardsizeyPROMPT = Number(prompt("Podaj wysokość planszy"));
 
-var styles = {
-  plusClass: {
-    display: "inline-block",
-    width: "50px",
-    height: "50px",
-    background:
-      "linear-gradient(#fff, #fff), linear-gradient(#fff, #fff), #000",
-    backgroundPosition: "center",
-    backgroundSize: "50% 2px, 2px 50%",
-    backgroundRepeat: "no-repeat",
-  },
-  altClass: {
-    background: "linear-gradient(#000, #000), linear-gradient(#000, #000)",
-    backgroundPosition: "center",
-    backgroundSize: "50% 2px, 2px 50%",
-    backgroundRepeat: "no-repeat",
-  },
-};
-
 function boardconstructor(maxboardsizex, maxboardsizey) {
   for (var x = 1; x <= maxboardsizex; x++) {
     for (var y = 1; y <= maxboardsizey; y++) {
       var div = document.createElement("div");
+      var plus = document.createElement("img");
+
+      plus.src = "plusBLACK.svg";
+      plus.style.maxWidth = "100%";
+      plus.style.position = "relative";
+      plus.id = "plusBLACK" + x + y;
+
       div.className = "cross" + x + y;
-      div.style.width = "100px";
-      div.style.height = "100px";
-      div.style.backgroundColor = "yellow";
+      div.id = "cross" + x + y;
+      div.style.width = "fit-content";
+      div.appendChild(plus);
+
       document.getElementById("board").appendChild(div);
       window["cross" + x + y] = [x, y];
+
+      $("img").hover(
+        function () {
+          $(this).css("background-color", "008cff ");
+        },
+        function () {
+          $(this).css("background-color", "white");
+        }
+      );
     }
   }
+  var board = document.getElementById("board");
+  board.style.gridTemplateRows = "repeat(" + maxboardsizey + ", 1fr)";
+  board.style.gridTemplateColumns = "repeat(" + maxboardsizex + ", 1fr)";
 }
+
 boardconstructor(maxboardsizexPROMPT, maxboardsizeyPROMPT);
+
+var blueturn = true;
+var orangeturn = false;
+$("img").click(function () {
+  if (blueturn == true) {
+    $(this).attr("src", "plusBLUE.svg");
+    orangeturn = true;
+    blueturn = false;
+    console.log(orangeturn, blueturn);
+  } else if (orangeturn == true) {
+    $(this).attr("src", "plusORANGE.svg");
+    blueturn = true;
+    orangeturn = false;
+    console.log(orangeturn, blueturn);
+  }
+});
